30ee3a57e6554d7a0bb1c1dbb6bafa1422649089
Zalfarsyh9 <145966652+Zalfarsyh9@users.noreply.github.com>
2023-10-26 12:47:23 +0700

    Update TI_REGPAGI22205011latihan23ZalfaJava.java

--- src/main/java/com/mycompany/ti_regpagi22205011latihan23/zalfa/java/TI_REGPAGI22205011latihan23ZalfaJava.java
+++ src/main/java/com/mycompany/ti_regpagi22205011latihan23/zalfa/java/TI_REGPAGI22205011latihan23ZalfaJava.java
@@ -7,6 +7,9 @@ import java.util.Scanner;
 /**
  *
  * @author User
+ Nama : Zalfa Rosiyah Riski 
+ Nim : 22205011
+ Jurusan : Teknik Informatika
  */
 public class TI_REGPAGI22205011latihan23ZalfaJava {
 
